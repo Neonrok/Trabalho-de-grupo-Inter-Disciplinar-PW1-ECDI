d985c9ca4c16b1214d7e6080f87cfaddf719a069
Rodrigo D <neonrok@pm.me>
2024-12-27 17:58:52 +0000

    ainda falta importar muita coisa eu só coluquei a paleta de cores e ajeitei o inicio

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,47 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+    import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+    <slot name="upper_bar"></slot>
+    <nav class="Barra_de_navegação">
+        <div class="Home_But">a</div>
+        <div class="Navig_Buts">b</div>
+        <div class="Profile_Buts">c</div>
+    </nav>
+    <slot></slot>
+    <div class="final">
+        <div>
+            <p>Copyright©</p>
+        </div>
     </div>
-  </header>
-
-  <RouterView />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
+<style>
+    :root{
+        --CWhite:#ECFEF3;
+        --CBlack:#0A0A16;
+        --CSecI:#FF931F;
+        --CSecII:#FFC07F;
+        --CPrin:#D62828;
+    }
+    .Barra_de_navegação{
+        background-color: var(--CSecI);
+        color: var(--CBlack);
+        padding: 5px;
+        padding-left: 30px;
+        padding-right: 30px;
+        display: flex;
+        justify-content: space-between;
+    }
+    .final{
+        background-color: var(--CSecI);
+        color: var(--CBlack);
+        margin-top: 0.3%;
+        padding: 5px;
+        padding-left: 10px;
+        padding-right: 10px;
+        display: flex;
+        flex-direction: row-reverse;
+    }
 </style>
