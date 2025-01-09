df4710b43b858c0b7660a72bd4cc18fbe4e6705a
Rodrigo D <neonrok@pm.me>
2025-01-09 21:31:43 +0000

    começo da home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,25 @@
-<script setup>
+<template>
+    <br>
+    <div class="secção">
+        <h1>a</h1>
+        <p class="linha"></p>
+        <div>
+            <>
+        </div>
+        <p class="linha"></p>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
 </script>
 
-<template>
-</template>
+<style lang="scss" scoped>
+    .linha{
+        background-color: #ECFEF3;
+        width: 100%;
+        height: 3px;
+    }
+</style>
